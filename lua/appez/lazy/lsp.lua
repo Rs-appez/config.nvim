7913a2875df089fca7c86e4b3e35e6e8a70424de
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2025-01-09 07:01:54 +0100

    add autocompletion/snippet

--- lua/appez/lazy/lsp.lua
+++ lua/appez/lazy/lsp.lua
@@ -1,28 +1,30 @@
 return {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+	},
 
-    },
-
-    config = function()
-        require("mason").setup()
-        require("mason-lspconfig").setup({
-            ensure_installed = {
-                "lua_ls",
-                "angularls",
-                "bashls",
-                "html",
-                "ts_ls",
-                "pyright"
-            },
-            handlers = {
-                function(server_name)
-                    require("lspconfig")[server_name].setup {}
-                end,
-            }
-        })
-        vim.keymap.set({ 'n' }, '<leader>ca', vim.lsp.buf.code_action, {})
-    end
+	config = function()
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
+		require("mason").setup()
+		require("mason-lspconfig").setup({
+			ensure_installed = {
+				"lua_ls",
+				"angularls",
+				"bashls",
+				"html",
+				"ts_ls",
+				"pyright",
+			},
+			handlers = {
+				function(server_name)
+					require("lspconfig")[server_name].setup({
+						capabilities = capabilities,
+					})
+				end,
+			},
+		})
+		vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
+	end,
 }
